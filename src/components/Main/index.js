2e0a7ec89e108f4ae3c5df04f64e0e8d72116310
gutieYpong <ponglai22@gmail.com>
2022-01-26 01:55:46 +0800

    app rewired.

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,8 +1,14 @@
 import { useState, useEffect } from "react";
 import { isEmpty } from "lodash";
 import styled from "styled-components";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 import useDataApi from "../../hooks/useDataAPI";
+
+
 
 const Container = styled.div`
   width: 100%;
@@ -106,6 +112,21 @@ const Main = props => {
     // fetchData,
   } = props;
 
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
   return (
     <Container>
@@ -129,6 +150,22 @@ const Main = props => {
           </QuoteContainer>
         ))
       }
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
     </Container>
   )
 }
